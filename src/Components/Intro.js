49ec63905afac13e1173ecb718f02dce2a2da133
Naeem Ul Hassan <2021cs667@student.uet.edu.pk>
2023-08-14 13:51:50 +0500

    ReactJS TODO App

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -7,6 +7,7 @@ function Intro() {
     const [Check, setCheck] = useState(false);
 
     const navigate = useNavigate();
+    
     const handleButtonClick = () => {
         setCheck(true);
         setTimeout(() => {
